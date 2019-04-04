6a8e7298ced10975051233cf0cb434f3361e9101
javatr6 <javafullstack.19sep@gmail.com>
2019-04-04 18:39:17 +0530

    main method updated

--- Demo/src/main/java/org/cap/boot/MainClass.java
+++ Demo/src/main/java/org/cap/boot/MainClass.java
@@ -5,6 +5,9 @@ import org.cap.model.Calculation;
 public class MainClass {
 
 	public static void main(String[] args) {
+		
+		System.out.println("Calculation Project....");
+		
 		int[] arr= {1,2,3,4,4,5,5,67};
 		Calculation obj=new Calculation();
 		obj.findSum(arr);
